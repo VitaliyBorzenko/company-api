7e44c4c72f77b434a5f2a4cacd3d1a04ee7affc9
VitaliyBorzenko <vborzenko1987@mail.ru>
2023-02-14 15:53:02 +0600

    initial commit

---
+++ src/main/java/feign/EmployeeFeign.java
@@ -0,0 +1,12 @@
+package feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("employee-core-api")
+
+public interface EmployeeFeign {
+    @GetMapping("/employee/check")
+    String checkEmployee();
+
+}
